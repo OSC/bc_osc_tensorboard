92ec3741f5bd839423aecc5fd63e6dc6ae38954a
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-02-04 15:25:35 -0500

    authrevproxy updates from original
    - can't compare users. unshare runs as 'root'
    - python3 updates so strings are bytes now
    - twisted library update (the import change and getCookie api change, etc)
    - need to be able to bind to 0.0.0.0 instead of just lo

--- template/bin/authrevproxy.py
+++ template/bin/authrevproxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # provides a reverse proxy that only allows requests coming from the same
 # user it's running as
@@ -11,12 +11,11 @@
 from twisted.internet import reactor, endpoints
 from twisted.web import proxy, server
 from twisted.web.resource import Resource
-from twisted.web.error import ForbiddenResource
+from twisted.web.resource import ForbiddenResource
 
 import argparse
 import getpass
 import sys, os
-
 
 #
 # ReverseProxy Resource that checks cookie before forwarding requests
@@ -31,38 +30,36 @@ class TokenResource(Resource):
 
     def getChild(self, path, request):
 
-        # get username from headers
-        # - this should be set on authenticated web sessions
-        try:
-            user = request.getHeader("x-forwarded-user")
-        except:
-            user = None
 
         # check if the request comes from the right user (note: can be forged)
-        if user == getpass.getuser():
+        # unshare is booted by "root" (what you think is root anyhow)
+        # if user == getpass.getuser():
 
-            # get ood token cookie
-            # - cookie name depends on the ood interactive session id ($PWD)
-            ood_session_id = os.path.basename(os.getcwd())
-            try:
-                cookie = request.getCookie('_ood_token_' + ood_session_id)
+        # get ood token cookie
+        # - cookie name depends on the ood interactive session id ($PWD)
+        ood_session_id = os.path.basename(os.getcwd())
+        cookie_name = str.encode('_ood_token_' + ood_session_id)
+        cookie_bytes = request.getCookie(cookie_name)
 
-            except:
-                cookie = None
+        # getCookie doesn't throw key error but could be None
+        if cookie_bytes != None:
+            cookie = cookie_bytes.decode()
+        else:
+            cookie = None
 
-            # get token from environment
-            # - $_ood_token_<session_id> is set in template/before.sh script
-            try:
-                ood_token = os.environ.get("_ood_token_" +
-                                           ood_session_id.replace("-", "_"))
-            except:
-                ood_token = None
+        # get token from environment
+        # - $_ood_token_<session_id> is set in template/before.sh script
+        try:
+            ood_token = os.environ.get("_ood_token_" +
+                                        ood_session_id.replace("-", "_"))
+        except:
+            ood_token = None
 
-            # check that cookie matches the local token
-            if cookie == ood_token and cookie != None:
-                return proxy.ReverseProxyResource(self.host,
-                                                  self.port,
-                                                  '/' + path)
+        # check that cookie matches the local token
+        if cookie == ood_token and cookie != None:
+            return proxy.ReverseProxyResource(self.host,
+                                                self.port,
+                                                b'/' + path)
 
         # no cheese for you
         request.setResponseCode(403)
@@ -78,6 +75,9 @@ def parse_args(argv=sys.argv[1:]):
                         required = True, help='application port, to forward requests to')
     parser.add_argument('--proxy-port', dest='proxy_port', type = int,
                         required = True, help='proxy port, to listen for requests on')
+    parser.add_argument('--bind-host', dest='bind_host', type = str,
+                required = False, help='the host to bind on', default="127.0.0.1")
+
     args = parser.parse_args(argv)
     return args
 
@@ -88,7 +88,7 @@ def main(argv=sys.argv[1:]):
     args = parse_args(argv)
     print('authrevproxy is starting on port {} -> {}'.format(args.proxy_port,
                                                              args.app_port) )
-    dest = TokenResource('127.0.0.1', args.app_port, '')
+    dest = TokenResource(args.bind_host, args.app_port, b'')
     site = server.Site(dest)
 
     endpoint = endpoints.TCP4ServerEndpoint(reactor, args.proxy_port)
@@ -98,4 +98,3 @@ def main(argv=sys.argv[1:]):
 
 if __name__ == '__main__':
     main()
-
